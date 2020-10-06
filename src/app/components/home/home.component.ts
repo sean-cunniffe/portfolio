bec3405a3ca3ad15cf9898b91ef442760df5b6bd
seanc <S.CUNNIFFE3@NUIGALWAY.IE>
2020-10-06 15:08:06 +0100

    0610

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import {Component, HostListener, OnInit} from '@angular/core';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  arrowDown = faAngleDown;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  nextSection() {
+    window.scroll({
+      top:window.innerHeight,
+      behavior: 'smooth'
+    })
+    console.log(window.pageYOffset);
+  }
+}
