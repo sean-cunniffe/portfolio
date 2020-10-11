f1197afdb4d85dd0fd8a54e1c04d81b52a324b87
sean-cunniffe <sean.cunniffe927@gmail.com>
2020-10-11 15:45:26 +0100

    11/10 home and about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  age: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.age = (+new Date()- +new Date("1997-11-02"))/31536000000;
+    this.age = Math.floor(+this.age);
   }
 
 }
