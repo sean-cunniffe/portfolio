9c8809461c223e546fc605392e3d006d25c6a7f5
sean-cunniffe <sean.cunniffe927@gmail.com>
2020-11-04 18:17:11 +0000

    quizzing system project updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Resume-Website';
+  title = 'Sean Cunniffe Portfolio';
 
   ngOnInit(): void {
-    const vh = window.innerHeight * 0.01;
-    console.log(vh);
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log(document.documentElement.style.getPropertyValue('--vh'));
+    // const vh = window.innerHeight * 0.01;
+    // console.log(vh);
+    // document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // console.log(document.documentElement.style.getPropertyValue('--vh'));
   }
 }
-
-window.addEventListener('resize', () => {
-    const vh = window.innerHeight * 0.01;
-    console.log(vh);
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log(vh);
+window.addEventListener('load', function () {
+   document.getElementById("content").style.visibility = "visible";
+  document.getElementById("loading-screen").style.visibility = "hidden";
 });
+
+// window.addEventListener('resize', () => {
+//     const vh = window.innerHeight * 0.01;
+//     console.log(vh);
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+//     console.log(vh);
+// });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
     imports: [
         BrowserModule,
